5cf2a5081cb7bd4aa94db6a31aea9d220e818999
Oleksandr Isaiev <oleksandr.isaiev@thetrainline.com>
2024-03-27 17:41:04 +0200

    init the project

---
+++ DubDubGrub/DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Oleksandr Isaiev on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
