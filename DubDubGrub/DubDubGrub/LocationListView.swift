5600c7d55ea82b31a4bbce19e10c503eb8b1eded
Oleksandr Isaiev <oleksandr.isaiev@thetrainline.com>
2024-03-28 12:14:35 +0200

    prepare location list view screen

--- DubDubGrub/DubDubGrub/LocationListView.swift
+++ DubDubGrub/DubDubGrub/LocationListView.swift
@@ -9,10 +9,51 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text("LocationListView")
+        NavigationStack {
+            List {
+                ForEach(0..<10) { item in
+                    HStack {
+                        Image(.defaultSquareAsset)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .padding(.vertical, 8)
+                        VStack(alignment: .leading) {
+                            Text("Test Location Name")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+                                .truncationMode(.middle)
+
+                            HStack {
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                            }
+                        }
+                        .padding(.leading)
+                    }
+                }
+            }
+            .navigationTitle("Grub Spots")
+        }
     }
 }
 
 #Preview {
     LocationListView()
+}
+
+struct AvatarView: View {
+    var body: some View {
+        Image(.defaultAvatar)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 35, height: 35)
+            .clipShape(Circle())
+    }
 }
